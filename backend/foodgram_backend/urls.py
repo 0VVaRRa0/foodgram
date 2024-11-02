8f9ad733bbbe396fcdf8718700233cdb22d069ef
Ivan Danilin <ioann.vorobey1029@gmail.com>
2024-11-02 11:07:30 +0100

    Создана кастомная модель и менеджер пользователей , исправлены пути в urls.py, обновлён gitignore, созданы миграции

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -7,7 +7,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('djoser.urls')),
-    path('api/', include('djoser.urls.authtoken')),
+    path('api/auth/', include('djoser.urls.authtoken')),
 ]
 
 if settings.DEBUG:
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,48 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(
+            self, username, email, first_name, last_name, password):
+        if not all([username, email, first_name, last_name, password]):
+            raise ValueError("All fields are required")
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+            self, username, email, first_name, last_name, password):
+        user = self.create_user(
+            username=username,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            password=password
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 class CustomUser(AbstractUser):
-    pass
+    username = models.CharField(max_length=150, unique=True, blank=False)
+    email = models.EmailField(unique=True, blank=False)
+    first_name = models.CharField(max_length=30, blank=False)
+    last_name = models.CharField(max_length=30, blank=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
+
+    class Meta:
+        ordering = ('id',)
