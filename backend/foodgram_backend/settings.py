1f1c2158c4dccfad0efb872b7e6d8c941c1fdd4b
VVaRRa <vvarra.work@gmail.com>
2024-11-19 15:11:36 +0100

    Добавлены DEFAULT_PERMISSION_CLASSES для DRF, в параметрах Djoser HISE_USERS установлен на False

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -84,13 +84,16 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
 
 DJOSER = {
     'LOGIN_FIELD': 'email',
+    'HIDE_USERS': False,
 }
 
 LANGUAGE_CODE = 'ru-RU'
