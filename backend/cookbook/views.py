b267f7057662a3e1fd0ba2eb6be70fa7cae6c534
VVaRRa <vvarra.work@gmail.com>
2024-11-21 16:24:05 +0100

    Написаны сериализаторы и вьюсеты для тегов и ингредиентов

--- backend/cookbook/views.py
+++ backend/cookbook/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 
+from .models import Tag, Ingredient
+from .serializers import TagSerializer, IngredienSerializer
+
+
+class TagViewSet(ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = (AllowAny,)
+    pagination_class = None
+
+
+class IngredientViewSet(ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredienSerializer
+    permission_classes = (AllowAny,)
+    pagination_class = None
--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -2,14 +2,21 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
+from cookbook.views import TagViewSet, IngredientViewSet
 from users.views import AvatarUpdate
+
+router = DefaultRouter()
+router.register('tags', TagViewSet)
+router.register('ingredients', IngredientViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/me/avatar/', AvatarUpdate.as_view()),
+    path('api/', include(router.urls)),
     path('api/', include('djoser.urls')),
+    path('api/users/me/avatar/', AvatarUpdate.as_view()),
     path('api/auth/', include('djoser.urls.authtoken')),
 ]
 
