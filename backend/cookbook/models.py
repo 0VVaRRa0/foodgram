6a04c7539ae326b2e607d70e28e27e42f388fe9c
VVaRRa <vvarra.work@gmail.com>
2024-11-21 15:57:07 +0100

    Созданы модели тегов и ингредиентов

--- backend/cookbook/models.py
+++ backend/cookbook/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField('Название', max_length=32)
+    slug = models.SlugField('Слаг', max_length=32)
+
+    class Meta:
+        default_related_name = 'tags'
+        ordering = ('id', 'name')
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField('Название', max_length=128)
+    measurement_unit = models.CharField('Единицы измерения', max_length=64)
+
+    class Meta:
+        default_related_name = 'ingredients'
+        ordering = ('id', 'name')
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return self.name
