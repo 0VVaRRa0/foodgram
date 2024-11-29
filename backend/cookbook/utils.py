6cdd75b59aa5380075cbb5e1de62aa50c3298173
VVaRRa <vvarra.work@gmail.com>
2024-11-29 15:52:55 +0100

    Добавлена загрузка ингредиентов из csv файла

--- backend/cookbook/utils.py
+++ backend/cookbook/utils.py
@@ -1,7 +1,10 @@
+import csv
+
 import pandas as pd
 from hashids import Hashids
 
 from .constants import SHORT_LINK_MIN_LENGTH
+from .models import Ingredient
 
 
 def generate_short_link(obj_id):
@@ -21,3 +24,12 @@ def generate_shopping_cart_file(ingredients):
         columns=['Ингредиент', 'Количество', 'Ед. измерения']
     )
     return df.to_csv(index=False)
+
+
+def load_ingredients():
+    with open('data/ingredients.csv', mode='r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            name, measurement_unit = row[0], row[1]
+            Ingredient.objects.create(
+                name=name, measurement_unit=measurement_unit)
