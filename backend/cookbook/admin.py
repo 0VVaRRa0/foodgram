6f6dd758cdb832b49a5efd0775f29495276809d7
VVaRRa <vvarra.work@gmail.com>
2024-11-29 20:05:25 +0100

    Настроена админ-зона проекта

--- backend/cookbook/admin.py
+++ backend/cookbook/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.db.models import Count
+
+from .models import Ingredient, Tag, Recipe
 
 
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author')
+    search_fields = ('name', 'author')
+    list_filter = ('tags',)
+    readonly_fields = ('favorites_count',)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(favorites_count=Count('favorite'))
+        return queryset
+
+    def favorites_count(self, obj):
+        return obj.favorites_count
+    favorites_count.admin_order_field = 'favorites_count'
+    favorites_count.short_description = 'Добавлений в избранное'
+
+
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Recipe, RecipeAdmin)
