385a577eccf081d64e12be232a9130360fbc516d
VVaRRa <vvarra.work@gmail.com>
2024-12-01 15:11:36 +0100

    Подправлена регистрация моделей в админ-зоне

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 
 User = get_user_model()
 
 
-class CustomUserAdmin(admin.ModelAdmin):
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
     list_display = ('email', 'username', 'first_name', 'last_name')
     search_fields = ('email', 'username')
-
-
-admin.site.register(User, CustomUserAdmin)
