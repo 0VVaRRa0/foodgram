1b4f430add7fe802b54cccfe77a4f867e3a6ef53
VVaRRa <vvarra.work@gmail.com>
2024-12-01 15:20:54 +0100

    Обновлён permissions

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -5,6 +5,6 @@ class IsOwnerOrAdminOrReadOnly(BasePermission):
     """Кастомный класс резрешений"""
     def has_object_permission(self, request, view, obj):
         user = request.user
-        if type(obj) is type(user) and obj == user:
+        if request.method in SAFE_METHODS or user.is_superuser:
             return True
-        return request.method in SAFE_METHODS or user.is_staff
+        return obj == user
