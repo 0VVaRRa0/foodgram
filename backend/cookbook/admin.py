444e60e36aedca3e96aa8d18b52151ee78cae9ee
VVaRRa <vvarra.work@gmail.com>
2024-12-04 16:55:03 +0100

    Исправлена админ-зона

--- backend/cookbook/admin.py
+++ backend/cookbook/admin.py
@@ -1,7 +1,8 @@
+from djaa_list_filter.admin import AjaxAutocompleteListFilterModelAdmin
 from django.contrib import admin
 from django.db.models import Count
 
-from .models import Ingredient, Recipe, Tag
+from .models import Ingredient, Recipe, Tag, ShoppingCart, Favorite
 
 
 @admin.register(Ingredient)
@@ -13,19 +14,41 @@ class IngredientAdmin(admin.ModelAdmin):
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
+    search_fields = ('name', 'slug')
+
+
+class BaseAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe')
+    search_fields = ('user', 'recipe')
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.select_related('user', 'recipe')
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(BaseAdmin):
+    pass
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(BaseAdmin):
+    pass
 
 
 @admin.register(Recipe)
-class RecipeAdmin(admin.ModelAdmin):
+class RecipeAdmin(AjaxAutocompleteListFilterModelAdmin):
     list_display = ('name', 'author')
-    search_fields = ('name', 'author')
-    list_filter = ('tags',)
+    search_fields = ('name',)
+    autocomplete_list_filter = ('tags', 'author')
     readonly_fields = ('favorites_count',)
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
         queryset = queryset.annotate(favorites_count=Count('favorites'))
-        return queryset
+        return queryset.select_related(
+            'author'
+        ).prefetch_related('tags', 'ingredients')
 
     def favorites_count(self, obj):
         return obj.favorites_count
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
+from .models import Subscription
+
 
 User = get_user_model()
 
@@ -9,4 +11,14 @@ User = get_user_model()
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     list_display = ('email', 'username', 'first_name', 'last_name')
-    search_fields = ('email', 'username')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('follower', 'following')
+    search_fields = ('follower', 'following')
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.select_related('followers', 'following')
